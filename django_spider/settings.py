0420358fa37f5d205bd54e51b0dd70abb013624c
YXY2829 <yxy2829@gmail.com>
2016-04-30 23:21:04 +0800

    add some urls and pictures
    爬取多个页面的图片，增加了多个图片

--- django_spider/settings.py
+++ django_spider/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_spider.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pic',
+        'USER':'root',
+        'PASSWORD':'yan31933',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- pic_blog/views.py
+++ pic_blog/views.py
@@ -3,14 +3,19 @@ from django.http import HttpResponse
 from django.template import loader,Context
 import requests
 from bs4 import BeautifulSoup
+import time
 # Create your views here.
+
 
 def home(request):
     t=loader.get_template('home.html')
-    res = requests.get('http://jandan.net/ooxx/page-1966#comments')
-    html = BeautifulSoup(res.text,'lxml')
     pic_indexs=[]
-    for index in html.select('#comments img'):
-        pic_indexs.append(index.get('src'))
+    urls=['http://jandan.net/ooxx/page-%d#comments'% i for i in range(1966,1955,-1)]
+    for url in urls:
+        res = requests.get(url)
+        html = BeautifulSoup(res.text,'lxml')
+        for index in html.select('#comments img'):
+            pic_indexs.append(index.get('src'))
+        time.sleep(0.5)
     c=Context({'pic_indexs':pic_indexs})
     return HttpResponse(t.render(c))
